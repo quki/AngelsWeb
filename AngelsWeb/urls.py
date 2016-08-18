b0ebc8436dd6a07c42d7f6b3b3de7bec1f4d306b
quki <quki09@gmail.com>
2016-08-18 17:23:20 +0900

    Refactor urls

--- AngelsWeb/urls.py
+++ AngelsWeb/urls.py
@@ -13,10 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
+from django.conf.urls import url
 from django.contrib import admin
+import blog.views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'', include('blog.urls')),
+    url(r'^$', blog.views.main_page, name="main_page"),
 ]
